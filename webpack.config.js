15e3d57b40e0dcfe16211cac82633c32e80aa951
huuhoc <hocph.212@gmail.com>
2019-11-13 03:28:11 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+let config = {
+  devtool: 'source-map',
+  mode: 'development',
+  entry: {
+    main: [
+      './_dev/js/main.js',
+      './_dev/css/theme.scss'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(sa|sc)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /.(png|svg|gif|jpg)(\?[a-z0-9=\.]+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[hash].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /.(woff(2)?|eot|ttf|otf|svg)(\?[a-z0-9=\.]+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[hash].[ext]',
+              outputPath: '',
+            }
+          }
+        ]
+      },
+      {
+        test : /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  externals: {
+    //prestashop: 'prestashop',
+    $: '$',
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+        filename: 'theme.css',
+    }),
+    new BrowserSyncPlugin(
+      // BrowserSync options
+      {
+        // browse to http://localhost:3000/ during development
+        host: 'localhost',
+        port: 3000,
+        files: ['./*.html', './css/*.css'],
+        server: { baseDir: ['./'] }
+      },
+      // plugin options
+      {
+        // prevent BrowserSync from reloading the page
+        // and let Webpack Dev Server take care of this
+        reload: true
+      }
+    )
+  ]
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      compress: {
+        sequences: true,
+        conditionals: true,
+        booleans: true,
+        if_return: true,
+        join_vars: true,
+        drop_console: true
+      },
+      output: {
+        comments: false
+      },
+      minimize: true
+    })
+  );
+}
+
+module.exports = config;
